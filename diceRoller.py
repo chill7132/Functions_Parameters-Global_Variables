0d8162ed92d8f62e242028243f49921881be7a9a
Callum <chill7132@sohamcollege.org.uk>
2014-05-22 16:35:05 +0100

    Update diceRoller.py
    i tried i couple of new things and i sorted out majority of the errors

--- diceRoller.py
+++ diceRoller.py
@@ -10,23 +10,47 @@ def rollnumber():
     print("rolling.....")
     global roll
     roll = random.randint(1,6)
-    show_dice()
+    print(roll)
 
 
 def show_dice(roll):
     if roll == 1:
         print(s1)
+        rollnumber()
     elif roll == 2:
         print(s2)
+        rollnumber()
     elif roll == 3:
         print(s3)
+        rollnumber()
     elif roll == 4:
         print(s4)
+        rollnumber()
     elif roll == 5:
         print(s5)
+        rollnumber()
     elif roll == 6:
         print(s6)
+        rollnumber()
 
+
+def asknumber():
+    answer = input("How times would you like to roll the dice>>> ")
+    if answer == 1:
+        print(roll)
+    elif answer == 2:
+        print(roll)
+        print(roll + 2)
+    elif answer == 3:
+        print(roll)
+        print(roll+3)
+        print(roll+1)
+        
+        
+rollnumber()
+time.sleep(1)
+show_dice(roll)
+                 
 
     rollnumber()
     time.sleep(1)
